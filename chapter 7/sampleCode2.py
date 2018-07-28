fd148efc8af359575ce26399e9344e8db7200ebc
David Taieb <data38777@gmail.com>
2018-07-28 15:39:47 -0400

    Update sampleCode2.py

--- chapter 7/sampleCode2.py
+++ chapter 7/sampleCode2.py
@@ -1,5 +1,7 @@
 from pyspark.sql.types import StringType, DateType
 from bs4 import BeautifulSoup as BS
+fieldnames = [f["name"] for f in field_metadata]
+transforms = {item['name']:item['transform'] for item in field_metadata if "transform" in item}
 field_metadata = [
     {"name": "created_at","type": DateType()},
     {"name": "text", "type": StringType()},
