8b80e8f3e33b6fdc6672ecee1f27e0b983b28241
David Taieb <data38777@gmail.com>
2018-07-28 23:03:46 -0400

    Update sampleCode40.py

--- chapter 8/sampleCode40.py
+++ chapter 8/sampleCode40.py
@@ -1,14 +1,14 @@
 @route(p_order="*",d_order="*",q_order="*")
-    def build_arima_model_screen(self, p_order, d_order, q_order):
-        #Build the arima model
-        self.train_set = self.parent_pixieapp.get_active_df()[:-14]
-        self.test_set = self.parent_pixieapp.get_active_df()[-14:]
-        self.arima_model = ARIMA(
-            self.train_set['Adj. Close'], dates=self.train_set['Date'], 
-            order=(int(p_order),int(d_order),int(q_order))
-        ).fit(disp=0)
-        self.residuals = self.arima_model.resid.describe().to_frame().reset_index()
-        return """
+def build_arima_model_screen(self, p_order, d_order, q_order):
+    #Build the arima model
+    self.train_set = self.parent_pixieapp.get_active_df()[:-14]
+    self.test_set = self.parent_pixieapp.get_active_df()[-14:]
+    self.arima_model = ARIMA(
+        self.train_set['Adj. Close'], dates=self.train_set['Date'], 
+        order=(int(p_order),int(d_order),int(q_order))
+    ).fit(disp=0)
+    self.residuals = self.arima_model.resid.describe().to_frame().reset_index()
+    return """
 <div class="page-header text-center">
     <h3>ARIMA Model succesfully created</h3>
 <div>
